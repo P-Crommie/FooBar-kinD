deb83d464b2fb314a9c05d57532c51ad8ae9634f
Emmanuel Cromwell <emmanuelcromwell@ymail.com>
2023-04-27 20:32:37 +0000

    test metalLB setup

---
+++ setup_metalLB.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
+
+kubectl wait --namespace metallb-system \
+                --for=condition=ready pod \
+                --selector=app=metallb \
+                --timeout=90s
+
+network inspect -f '{{.IPAM.Config}}' kind
+
+kubectl apply -f LB/metalLB.yml
