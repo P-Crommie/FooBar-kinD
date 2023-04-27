4ced84e513f971c399c0e768870473ee01fe46b7
Emmanuel Cromwell <emmanuelcromwell@ymail.com>
2023-04-27 19:37:02 +0000

    test basic structure of workflow

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,13 @@
 echo "***Creating deployment***"
 
 kubectl create namespace ingress
+kubectl create namespace http-echo
 
-helm install ingress chart/nginx-ingress-controller-9.5.1.tgz --values chart/values-ingress.yml --namespace ingress
+helm install --namespace ingress ingress charts/nginx-ingress-controller-9.5.1.tgz --values charts/values-ingress.yml
 
 for resource in k8s/*
 do
-    kubectl apply -f $resource
+    kubectl apply -f $resource -n http-echo
 done
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
@@ -17,3 +18,5 @@ kubectl wait --namespace metallb-system \
                 --for=condition=ready pod \
                 --selector=app=metallb \
                 --timeout=90s
+
+kubectl apply -f LB/metalLB.yml
